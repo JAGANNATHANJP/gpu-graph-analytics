4ee35258ee49ffc578d1efa32e78775b5c35ab4b
JAGANNATHANJP <jpjagannathan07@gmail.com>
2026-06-03 22:21:41 +0530

    Add Graph class definition with basic methods

---
+++ include/graph.hpp
@@ -0,0 +1,20 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+
+class Graph
+{
+public:
+    Graph(int vertices);
+
+    void addEdge(int src, int dst);
+
+    void printGraph();
+
+private:
+    int numVertices;
+    std::vector<std::vector<int>> adjList;
+};
+
+#endif
