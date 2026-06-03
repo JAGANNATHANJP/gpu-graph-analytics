cb0d8c7447232587c6b0105620a8a9896e75a8e0
JAGANNATHANJP <jpjagannathan07@gmail.com>
2026-06-03 22:23:45 +0530

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "../include/graph.hpp"
+
+int main()
+{
+    Graph g(6);
+
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(3,5);
+    g.addEdge(4,5);
+
+    g.printGraph();
+
+    return 0;
+}
