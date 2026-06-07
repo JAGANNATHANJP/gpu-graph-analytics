d08ea6c44a39a9712b32250380c824123931db10
JAGANNATHANJP <jpjagannathan07@gmail.com>
2026-06-07 15:05:15 +0530

    Replace edge additions with graph loading from file

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,7 @@ int main()
 {
     Graph g(6);
 
-    g.addEdge(0,1);
-    g.addEdge(0,2);
-    g.addEdge(1,3);
-    g.addEdge(2,4);
-    g.addEdge(3,5);
-    g.addEdge(4,5);
+    g.loadGraph("datasets/sample_graph.txt");
 
     g.printGraph();
 
